7eca2b08d28c1fa781825fb7d59c91a750b2504c
Rohit Prasad <rohitprasad267@gmail.com>
2020-02-02 11:00:27 +0530

    02-02-2020

--- TinyUrl/src/app/main-body/main-body.component.ts
+++ TinyUrl/src/app/main-body/main-body.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../login.service';
+import { Component, OnInit } from "@angular/core";
+import { LoginService } from "../login.service";
 
 @Component({
-  selector: 'app-main-body',
-  templateUrl: './main-body.component.html',
-  styleUrls: ['./main-body.component.css']
+  selector: "app-main-body",
+  templateUrl: "./main-body.component.html",
+  styleUrls: ["./main-body.component.css"]
 })
 export class MainBodyComponent implements OnInit {
-
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService) {}
 
   ngOnInit() {
     const dataFound = localStorage.getItem('URLData');
@@ -24,44 +23,68 @@ export class MainBodyComponent implements OnInit {
     console.log('Long URL', this.longURL);
     if (!this.longURL) {
       alert('Give Some input');
-    }
-    const dataFound = localStorage.getItem('URLData');
-    if (dataFound) {
-      const URLArray = JSON.parse(dataFound);
-      const URL = URLArray.find((URL: { user: string, longURL: string, shortURL: string }) => URL.longURL == this.longURL);
-      if (URL) {
-        alert('Short URL Already created...');
-
+    } else {
+      const dataFound = localStorage.getItem('URLData');
+      if (dataFound) {
+        const URLArray = JSON.parse(dataFound);
+        const URL = URLArray.find(
+          (URL: { user: string; longURL: string; shortURL: string }) =>
+            URL.longURL == this.longURL
+        );
+        if (URL) {
+          alert('Short URL Already created...');
+          this.shortURL = URL.shortURL;
+          if (this.loginService.loggedUserName != URL.user) {
+            this.loginService.URLData.push({
+              user: this.loginService.loggedUserName,
+              longURL: this.longURL,
+              shortURL: this.shortURL
+            });
+            localStorage.setItem(
+              'URLData',
+              JSON.stringify(this.loginService.URLData)
+            );
+          }
+        } else {
+          this.shortURLConverter();
+          localStorage.setItem(
+            'URLData',
+            JSON.stringify(this.loginService.URLData)
+          );
+        }
       } else {
         this.shortURLConverter();
-        localStorage.setItem('URLData', JSON.stringify(this.loginService.URLData));
-
+        localStorage.setItem(
+          'URLData',
+          JSON.stringify(this.loginService.URLData)
+        );
       }
-    } else {
-      this.shortURLConverter();
-      localStorage.setItem('URLData', JSON.stringify(this.loginService.URLData));
-
     }
   }
 
   shortURLConverter() {
-    this.shortURL = 'https://rohitshortURL/' + this.makeid(9);
-    this.loginService.URLData.push({ user: this.loginService.loggedUserName, longURL: this.longURL, shortURL: this.shortURL});
-    console.log('URL Data' + this.loginService.URLData.values);
+    this.shortURL = "https://rohitshortURL/" + this.makeid(9);
+    this.loginService.URLData.push({
+      user: this.loginService.loggedUserName,
+      longURL: this.longURL,
+      shortURL: this.shortURL
+    });
+    console.log("URL Data" + this.loginService.URLData.values);
   }
 
   makeid(length: number) {
-    let result           = '';
-    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
- }
+  }
 
- doClear() {
-   this.longURL = '';
-   this.shortURL = '';
- }
+  doClear() {
+    this.longURL = "";
+    this.shortURL = "";
+  }
 }
